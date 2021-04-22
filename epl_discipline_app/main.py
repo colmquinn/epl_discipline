c456a4141b8379a5aff738102df0188c9ff74384
colmquinn <colm.declan.quinn@gmail.com>
2021-04-22 21:39:31 +0100

    add color by position

--- epl_discipline_app/main.py
+++ epl_discipline_app/main.py
@@ -4,6 +4,8 @@ from bokeh.plotting import ColumnDataSource, figure, curdoc
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from bokeh.models import Slope
+from bokeh.palettes import Category10
+from bokeh.transform import factor_cmap
 
 discipline = pd.read_csv(join(dirname(__file__), 'data/epl_discipline.csv'))
 
@@ -13,8 +15,11 @@ source = ColumnDataSource(data=dict(
     desc=discipline['player_name'],
     yellows=discipline['total_yellow'],
     reds=discipline['total_red'],
-    redsize=discipline['redsize']
+    redsize=discipline['redsize'],
+    position=discipline['position_mapped']
 ))
+
+color_map = factor_cmap('position', palette=Category10[4], factors=discipline['position_mapped'].unique())
 
 TOOLTIPS = [
     ("player", "@desc"),
@@ -39,7 +44,7 @@ regression_line = Slope(gradient=slope, y_intercept=intercept, line_color="red")
 p = figure(plot_width=800, plot_height=400, tooltips=TOOLTIPS,
            title="EPL Discipline", x_axis_label="Minutes played", y_axis_label="Yellow cards")
 
-p.circle('x', 'y', size='redsize', fill_alpha=0.5, source=source)
+p.circle('x', 'y', size='redsize', fill_alpha=0.5, source=source, line_color='white', fill_color=color_map, legend_field='position')
 p.add_layout(regression_line)
 curdoc().add_root(p)
 curdoc().title = "EPL Discipline"
